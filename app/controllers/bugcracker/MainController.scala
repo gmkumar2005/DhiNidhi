b941dcdd49db32dc55fc2378dd7135da6705eb60
Kiran Kumar <gmkumar2005@gmail.com>
2018-12-15 21:43:54 +0530

    Enhanced config, removed stop words, single client connection

--- app/controllers/bugcracker/MainController.scala
+++ app/controllers/bugcracker/MainController.scala
@@ -23,6 +23,7 @@ import cats.implicits._
 import util.Logging
 import com.sksamuel.elastic4s.http.ElasticDsl._
 import com.sksamuel.elastic4s.searches.sort.SortOrder
+import play.api.inject.ApplicationLifecycle
 
 import scala.concurrent.Future
 //import BgbugReader._
@@ -33,20 +34,30 @@ import cats.Show
 import scala.reflect.internal.util.Collections._
 
 @javax.inject.Singleton
-class MainController @javax.inject.Inject() (override val app: Application) extends BaseController("main") {
+class MainController @javax.inject.Inject() (override val app: Application, config: Configuration, appLifecycle: ApplicationLifecycle) extends BaseController("main") {
 
   import app.contexts.webContext
-  val client = HttpClient(ElasticsearchClientUri("10.91.10.13", 9200))
+  val client = HttpClient(ElasticsearchClientUri(
+    config.get[String]("dhiNidhi.elasticServer"),
+    config.get[Int]("dhiNidhi.elasticServerPort")))
+  val esIndex = config.get[String]("dhiNidhi.index")
+  val elasticServer = config.get[String]("dhiNidhi.elasticServer")
+  val elasticServerPort = config.get[Int]("dhiNidhi.elasticServerPort")
+
+  appLifecycle.addStopHook { () =>
+    Future.successful(client.close())
+  }
+
+  //  val esIndex = "defects"
   def index = withSession("admin.index", admin = true) { implicit request => implicit td =>
     Future.successful(Ok(views.html.bugcracker.index(request.identity, app.config.debug)))
-
   }
 
   def searchES = withSession("admin.index", admin = true) { implicit request => implicit td => {
     import BgbugReader.BgbugReader
 
     val limit = 10
-    val query = search("defects") query "test" limit {
+    val query = search(esIndex) query "test" limit {
       limit
     }
 
@@ -63,10 +74,9 @@ class MainController @javax.inject.Inject() (override val app: Application) exte
 
   def getbyid(id: String) = withSession("admin.index", admin = true) { implicit request => implicit td => {
     import BgbugReader.BgbugReader
-    val client = HttpClient(ElasticsearchClientUri("10.91.10.13", 9200))
 
     val resp = client.execute {
-      get(id) from "defects"
+      get(id) from esIndex
     }.map {
       case Left(s) => NotFound
       case Right(i) => {
@@ -80,10 +90,10 @@ class MainController @javax.inject.Inject() (override val app: Application) exte
 
   def searchBG = withSession("admin.index", admin = true) { implicit request => implicit td => {
     import BgbugReader.BgbugReader
+
     val esQuery = decode[ESQuery](request.body.asText.getOrElse("{}"))
     val recievedEsQuery = esQuery.getOrElse(ESQuery("", 0, 15))
-    val client = HttpClient(ElasticsearchClientUri("10.91.10.13", 9200))
-    val query = search("defects") query { recievedEsQuery.query } limit { recievedEsQuery.limit } start
+    val query = search(esIndex) query { recievedEsQuery.query } limit { recievedEsQuery.limit } start
       { recievedEsQuery.start } sortByFieldDesc ("Defect ID")
 
     val resp = client.execute(query).map {
@@ -93,6 +103,7 @@ class MainController @javax.inject.Inject() (override val app: Application) exte
         Ok(SearchResult(i.result.totalHits, distinctItems).asJson)
       }
     }
+
     resp
   }
   }
@@ -105,10 +116,10 @@ class MainController @javax.inject.Inject() (override val app: Application) exte
     // dont remove    val queryFuture: Future[Either[RequestFailure, RequestSuccess[SearchResponse]]] = client.execute(query)
 
     val relatedEsResults = for {
-      r1 <- EitherT(client.execute(get(recievedEsQuery.query) from "defects"))
+      r1 <- EitherT(client.execute(get(recievedEsQuery.query) from esIndex))
       r2 <- {
         val fullDoc = r1.result.to[Bgbug]
-        val releatedEsQuery = search("defects") query removeStopWords(fullDoc.`Description` + fullDoc.`Summary`) limit { 15 }
+        val releatedEsQuery = search(esIndex) query removeStopWords(fullDoc.`Description` + fullDoc.`Summary`) limit { 15 }
         EitherT(client.execute(releatedEsQuery))
       }
     } yield r2
@@ -137,10 +148,10 @@ class MainController @javax.inject.Inject() (override val app: Application) exte
     //    val queryFuture: Future[Either[RequestFailure, RequestSuccess[SearchResponse]]] = client.execute(query)
 
     val relatedEsResults = for {
-      r1 <- EitherT(client.execute(get(recievedEsQuery.query) from "defects"))
+      r1 <- EitherT(client.execute(get(recievedEsQuery.query) from esIndex))
       r2 <- {
         val fullDoc = r1.result.to[Bgbug]
-        val relatedEsNFRQuery = search("defects") query appendStatusQuery(fullDoc.`Summary`) limit { 10 }
+        val relatedEsNFRQuery = search(esIndex) query appendStatusQuery(fullDoc.`Summary`) limit { 10 }
         //        println(client.show(relatedEsNFRQuery))
         log.debug("relatedEsNFRQuery")
         log.debug(client.show(relatedEsNFRQuery))
@@ -171,10 +182,10 @@ class MainController @javax.inject.Inject() (override val app: Application) exte
     //    val queryFuture: Future[Either[RequestFailure, RequestSuccess[SearchResponse]]] = client.execute(query)
 
     val relatedEsResults = for {
-      r1 <- EitherT(client.execute(get(recievedEsQuery.query) from "defects"))
+      r1 <- EitherT(client.execute(get(recievedEsQuery.query) from esIndex))
       r2 <- {
         val fullDoc = r1.result.to[Bgbug]
-        val relatedEsNFRQuery = search("defects") query removeStopWords(fullDoc.`Summary`) + """ AND Status.keyword: "Closed" """ limit { 10 }
+        val relatedEsNFRQuery = search(esIndex) query removeStopWords(fullDoc.`Summary`) + """ AND Status.keyword: "Closed" """ limit { 10 }
         //        println(client.show(relatedEsNFRQuery))
         log.debug("relatedEsNFRQuery")
         log.debug(client.show(relatedEsNFRQuery))
@@ -200,19 +211,9 @@ class MainController @javax.inject.Inject() (override val app: Application) exte
     removeStopWords(queryWords) + """ AND Status.keyword: "Closed NFR" """
   }
   def removeStopWords(sentence: String): String = {
-    val stopWords = Set("gpp", "i", "me", "my", "myself", "we", "our", "ours", "ourselves", "you", "youre", "youve", "youll",
-      "youd", "your", "yours", "yourself", "yourselves", "he", "him", "his", "himself", "she", "shes", "her", "hers",
-      "herself", "it", "its", "its", "itself", "they", "them", "their", "theirs", "themselves", "what", "which", "who", "whom",
-      "this", "that", "thatll", "these", "those", "am", "is", "are", "was", "were", "be", "been", "being", "have", "has", "had", "having",
-      "do", "does", "did", "doing", "a", "an", "the", "and", "but", "if", "or", "because", "as", "until", "while", "of", "at", "by", "for", "with",
-      "about", "against", "between", "into", "through", "during", "before", "after", "above", "below", "to", "from", "up", "down", "in", "out", "on",
-      "off", "over", "under", "again", "further", "then", "once", "here", "there", "when", "where", "why", "how", "all", "any", "both", "each", "few",
-      "more", "most", "other", "some", "such", "no", "nor", "not", "only", "own", "same", "so", "than", "too", "very", "s", "t", "can", "will", "just", "don",
-      "dont", "should", "shouldve", "now", "d", "ll", "m", "o", "re", "ve", "y", "ain", "aren", "arent", "couldn", "couldnt", "didn", "didnt", "doesn", "doesnt",
-      "hadn", "hadnt", "hasn", "hasnt", "haven", "havent", "isn", "isnt", "ma", "mightn", "mightnt", "mustn", "mustnt", "needn", "neednt", "shan", "shant",
-      "shouldn", "shouldnt", "wasn", "wasnt", "weren", "werent", "won", "wont", "wouldn", "wouldnt")
-    val trimmedSentance = sentence.filter(_ >= ' ').trim.replaceAll(" +", " ").replaceAll("[^A-Za-z0-9\\s]", "").toLowerCase
-    val wordList = trimmedSentance.split(" ")
+    val stopWords = Set("gpp", "msg", "payment")
+    val trimmedSentence = sentence.filter(_ >= ' ').trim.replaceAll(" +", " ").replaceAll("[^A-Za-z0-9\\s]", "").toLowerCase
+    val wordList = trimmedSentence.split(" ")
     val cleanList = wordList.filter(!stopWords.contains(_))
     cleanList.mkString(" ")
   }
@@ -221,9 +222,8 @@ class MainController @javax.inject.Inject() (override val app: Application) exte
 
   def serverStatus = withSession("admin.index", admin = true) { implicit request => implicit td => {
     import BgbugReader.BgbugReader
-    val client = HttpClient(ElasticsearchClientUri("10.91.10.13", 9200))
     val limit = 10
-    val query = search("defects") query "test" limit {
+    val query = search(esIndex) query "test" limit {
       limit
     }
     val resp = client.execute(query).map {
